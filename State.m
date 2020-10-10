4529cd56b89f3e1c37eb1de7ea94964e62d537cd
Sreevalsan Sanathanan Menon <34897964+sreevalsansmenon@users.noreply.github.com>
2020-10-10 17:37:13 -0500

    Add files via upload

---
+++ State.m
@@ -0,0 +1,26 @@
+classdef State<handle
+    properties
+        Country_Name="";
+        Daily_Totals
+        Cummulative_Deaths
+        Cummulative_Cases
+        Daily_Deaths
+        Daily_Cases
+    end
+    
+    methods
+        function obj = State(name,covid_data)
+            if nargin==0
+                return
+            end
+            obj.Country_Name = name;
+            obj.Daily_Totals =  cell2mat(covid_data(ismember(covid_data(2:end,2),name),3:end));
+            obj.Cummulative_Cases = sum(obj.Daily_Totals(:,1:2:end),1);
+            obj.Daily_Cases = [0 diff(obj.Cummulative_Cases)];
+            obj.Cummulative_Deaths= sum(obj.Daily_Totals(:,2:2:end),1);
+            obj.Daily_Deaths = [0 diff(obj.Cummulative_Deaths)];
+        end
+    end
+   
+        
+ end
